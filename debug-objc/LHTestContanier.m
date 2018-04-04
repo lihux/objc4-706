7ba69e428bd4784e779c49d32a7b60e070e79fdb
李辉 <lihui.zl@163.com>
2018-04-04 18:59:50 +0800

    探索KVO的实现原理

--- debug-objc/LHTestContanier.m
+++ debug-objc/LHTestContanier.m
@@ -13,6 +13,12 @@
 #import <objc/objc-runtime.h>
 #import <objc/runtime.h>
 
+@interface LHTestContanier ()
+
+@property (nonatomic, strong) Dog *dog;
+
+@end
+
 @implementation LHTestContanier
 
 - (void)beginTest {
@@ -29,8 +35,37 @@
     NSLog(@"打印一下数字a3地址吧:%p", a3);
     NSLog(@"打印一下数字a100地址吧:%p", a100);
     NSLog(@"打印一下数字xxx地址吧:%p", xxx);
+    self.dog = [Dog new];
+    [self printAllClasses];
+    [self.dog addObserver:self forKeyPath:@"legs" options:NSKeyValueObservingOptionNew context:nil];
+    NSLog(@"\n\n再次打印所有的类：\n\n");
+    [self printAllClasses];
     [Dog sayDog];
-    [Dog new];
+}
+
+- (void)printAllClasses {
+    Class *buff;
+    int classCount = objc_getClassList(NULL, 0);
+    buff = (Class *)malloc(classCount * sizeof(Class));
+    objc_getClassList(buff, classCount);
+    NSLog(@"%zd", classCount);
+    for (int i = 0; i < classCount; i++) {
+        Class cls = buff[i];
+        NSString *clsName = NSStringFromClass(cls);
+        if ([clsName containsString:@"Dog"]) {
+            NSLog(@"%@", clsName);
+        }
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"legs"]) {
+        NSLog(@"");
+    }
+}
+
+- (void)dealloc {
+    [self.dog removeObserver:self forKeyPath:@"legs"];
 }
 
 @end
